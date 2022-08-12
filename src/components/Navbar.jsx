3492e95a1ffd89ee4b297cf1730a8dd1a0e41a13
Victor Micheni <Victormichenim@gmail.com>
2022-08-12 15:01:20 +0300

    Bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,9 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} className="logo"></img>
+        /** we made a comment here to show this nigga must learn how to use
+        documentat */
+        <img src={Logo} alt="logo" className="logo"></img>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu</Link>
